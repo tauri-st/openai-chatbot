b434041f2b46d45959ffb50e74a4080f8c5fd9bb
tauri_s <tauristclaire@gmail.com>
2024-08-01 15:58:01 -0700

    Add input function to ask user to input text

--- chatbot.py
+++ chatbot.py
@@ -3,3 +3,6 @@ from openai import OpenAI
 #Create an instance of the API library
 #OpenAI() will automatically pull the key from the system variable
 client = OpenAI()
+
+#ask user to input text. This script is run via the command line for now so the input() function is required
+user_input = input("\nAsk me something...\n\n")
