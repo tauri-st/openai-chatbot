e5a6dc3880091bce41b5135515766d49579df329
tauri_s <tauristclaire@gmail.com>
2024-08-05 16:13:55 -0700

    Reformat comments and create outer structure of function to create chat completion

--- chatbot.py
+++ chatbot.py
@@ -1,8 +1,15 @@
 from openai import OpenAI
 
-#Create an instance of the API library
-#OpenAI() will automatically pull the key from the system variable
+# Create an instance of the API library
+# OpenAI() will automatically pull the key from the system variable
 client = OpenAI()
 
-#ask user to input text. This script is run via the command line for now so the input() function is required
+# ask user to input text. This script is run via the command line 
+# for now so the input() function is required
 user_input = input("\nAsk me something...\n\n")
+
+# Chat Completions API, formatted as an object where model and
+# messages are required
+response = client.chat.completions.create(
+
+)
